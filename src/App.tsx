9e514ff6d9a391aa318cc3227af34876cc52490c
pantpad <deathmatchplayerp@gmail.com>
2024-05-11 21:10:03 +0200

    feat: add pokemonLoading component :

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ const endpoint = "https://pokeapi.co/api/v2/pokemon";
 
 import ActionButtons from "./components/ActionButtons";
 import PokemonCard from "./components/PokemonCard";
-import PokemonError from "./components/PokemonError";
 
 function App() {
   const [id, setId] = useState(1);
+
   const {
     data: pokemon,
     isLoading,
@@ -22,11 +22,7 @@ function App() {
   return (
     <>
       <h1>Why react query?</h1>
-      {error ? (
-        <PokemonError />
-      ) : (
-        <PokemonCard isLoading={isLoading} data={pokemon} />
-      )}
+      <PokemonCard isLoading={isLoading} data={pokemon} error={error} />
       <ActionButtons setId={setId} />
     </>
   );
