5e8f57a66c2d43c72377b835f75fb7b33711e6e8
pantpad <deathmatchplayerp@gmail.com>
2024-05-11 18:54:51 +0200

    feat: add race condition checker inside fetch hook

--- src/hooks/useFetchPokemon.tsx
+++ src/hooks/useFetchPokemon.tsx
@@ -4,23 +4,36 @@ import { useState, useEffect, useRef } from "react";
 
 export function useFetchPokemon(id: number) {
   const [pokemon, setPokemon] = useState<PokeAPI.Pokemon | null>(null);
-  const [error, setError] = useState<any>();
+  const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+
+  const controller = useRef<AbortController | null>(null);
 
   useEffect(() => {
     async function fetchPokemon() {
+      controller.current?.abort();
+      controller.current = new AbortController();
+
       setIsLoading(true);
+      setPokemon(null);
+
       try {
-        setPokemon(null);
-        setError(false);
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        setError(null);
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${id}`,
+          { signal: controller.current?.signal }
+        );
 
         if (!response.ok) {
           throw new Error("Error getting pokemon");
         }
+
         const data = await response.json();
         setPokemon(data);
-      } catch (error) {
+      } catch (error: any) {
+        if (error.name === "AbortError") {
+          return;
+        }
         setError(error);
       }
 
