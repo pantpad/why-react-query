7a929ca55491cff990f23ef3a55b67d8a206efac
pantpad <deathmatchplayerp@gmail.com>
2024-05-12 18:03:46 +0200

    feat: return provider w/value

--- src/store/pokemon-context.tsx
+++ src/store/pokemon-context.tsx
@@ -2,6 +2,12 @@ import { createContext } from "react";
 
 const PokemonContext = createContext({});
 
-export default function PokemonContextProvider() {
-  return;
+export default function PokemonContextProvider({ children }) {
+  const pokemonContextStore = {};
+
+  return (
+    <PokemonContext.Provider value={pokemonContextStore}>
+      {children}
+    </PokemonContext.Provider>
+  );
 }
